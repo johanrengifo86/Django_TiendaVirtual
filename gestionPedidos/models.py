42291872647e9153d259d77181f6b5b4be578b78
Johan Rengifo <wrengifo@unicauca.edu.co>
2022-08-12 15:27:01 -0500

    retorno de modelos en cadena de caracteres

--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -14,6 +14,10 @@ class Articulos(models.Model):
     seccion = models.CharField(max_length=20)
     precio = models.IntegerField()
 
+    def __str__(self):
+        return 'El nombre es %s la sección es %s y el precios es: $ %s' %(self.nombre, self.seccion, self.precio)
+
+
 class Pedidos(models.Model):
     numero = models.IntegerField()
     fecha = models.DateField()

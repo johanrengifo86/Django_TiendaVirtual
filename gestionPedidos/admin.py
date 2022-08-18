0d5a4b2560ea9247089bca050b4c1a2b340b2941
Johan Rengifo <wrengifo@unicauca.edu.co>
2022-08-18 13:25:04 -0500

    Se agrega campo de busqueda

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -4,6 +4,10 @@ from gestionPedidos.models import Clientes, Articulos, Pedidos
 
 # Register your models here.
 
-admin.site.register(Clientes)
+class Clientes_Admin(admin.ModelAdmin):
+    list_display=("nombre", "direccion", "telefono")
+    search_fields = ("nombre", "telefono")
+
+admin.site.register(Clientes, Clientes_Admin)
 admin.site.register(Articulos)
 admin.site.register(Pedidos)
